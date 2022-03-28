b840f274800c170b71bb3e640446d914de64da57
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-28 21:21:02 +0700

    add inert di server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
 const Jwt = require('@hapi/jwt');
 const path = require('path');
 
@@ -75,6 +76,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
