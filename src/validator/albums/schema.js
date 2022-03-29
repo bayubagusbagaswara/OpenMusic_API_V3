6ecd6d8b6bcb34dafefc0dc0cc03a58aa84516ae
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-29 19:33:52 +0700

    update validaotr albums

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,13 +1,10 @@
 const Joi = require('joi');
 
-const currentYear = new Date().getFullYear();
-
 const AlbumPayloadSchema = Joi.object({
 
   name: Joi.string().required(),
-  year: Joi.number().integer().min(1900).max(currentYear)
+  year: Joi.number().integer().min(1900).max(new Date().getFullYear())
     .required(),
-  coverUrl: Joi.string(),
 });
 
 module.exports = { AlbumPayloadSchema };
